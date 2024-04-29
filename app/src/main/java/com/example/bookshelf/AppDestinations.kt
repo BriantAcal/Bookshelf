56f58f62028e65670a663dd18eff6c845a4555e6
Briant <al053030@uacam.mx>
2024-04-29 17:09:05 -0600

    first commit

---
+++ app/src/main/java/com/example/bookshelf/AppDestinations.kt
@@ -0,0 +1,8 @@
+package com.example.bookshelf
+
+enum class AppDestinations(val title: String) {
+    MenuScreen(title = "Menu"),
+    QueryScreen(title = "Google Bookshelf"),
+    FavoriteScreen(title = "Mis Libros Favoritos"),
+    DetailScreen(title = "Libro: ")
+}
